1b61a00f1b257e153152df1dbd9af1c1259ebdd9
larsi3016 <128397754+larsi3016@users.noreply.github.com>
2023-11-05 23:52:16 +0500

    Add files via upload

---
+++ buns/mod6/task3.py
@@ -0,0 +1,17 @@
+def armstrong_generator():
+    n = 2
+    while True:
+        digits = [int(x) for x in str(n)]
+        num_digits = len(digits)
+        sum_of_powers = sum([x**num_digits for x in digits])
+        if sum_of_powers == n and n not in range(2, 10):
+            yield n
+        n+=1
+
+generator = armstrong_generator()
+
+def get_armstrong_numbers():
+    return next(generator)
+
+for i in range(8):
+    print(get_armstrong_numbers(), end=' ')
